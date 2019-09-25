e28fac17654d845d6aa04b863e58e40333a1f0e4
simon3000 <simon3000@163.com>
2019-09-25 05:39:43 +0200

    pull and excute task
    https://github.com/dd-center/vtbs.moe/issues/79

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const WebSocket = require('ws')
+const got = require('got')
+
+const parse = string => {
+  try {
+    const json = JSON.parse(string)
+    if (json) {
+      const { key, data: { type, url } } = json
+      if (type === 'http') {
+        return { key, url }
+      }
+    }
+  } catch (_) {
+    return undefined
+  }
+}
 
 const url = process.env.development ? 'ws://0.0.0.0:9013' : 'wss://cluster.vtbs.moe'
+const PARALLEL = 8
+const INTERVAL = 320
 
 if (process.env.development) {
   console.log('Development Environment Detected')
@@ -11,8 +28,26 @@ console.log(`using: ${url}`)
 const connect = () => {
   const ws = new WebSocket(url)
 
+  ws.on('message', async message => {
+    const json = parse(message)
+    if (json) {
+      const { key, url } = json
+      console.log('job receive')
+      setTimeout(() => ws.send('DDhttp'), INTERVAL * PARALLEL)
+      const time = Date.now()
+      const { body } = await got(url, { json: true }).catch(() => ({}))
+      console.log(`job complete ${((Date.now() - time) / 1000).toFixed(2)}s`)
+      ws.send(JSON.stringify({
+        key,
+        data: body
+      }))
+    }
+  })
+
   ws.on('open', () => {
     console.log('DD@Home connected')
+    ws.send('DDhttp')
+    Array(PARALLEL).fill().map(() => ws.send('DDhttp'))
   })
 
   ws.on('error', e => {
@@ -21,11 +56,7 @@ const connect = () => {
 
   ws.on('close', n => {
     console.log(`closed ${n}`)
-    connect()
-  })
-
-  ws.on('message', data => {
-    console.log(data)
+    setTimeout(connect, 1000)
   })
 }
 
