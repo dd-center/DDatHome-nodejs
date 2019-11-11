932ffae8323701dc8517fd36594e75af8a343fee
simon3000 <simon30002021@icloud.com>
2019-11-11 08:12:35 +0100

    use core.js
    #3

---
+++ core.js
@@ -0,0 +1,154 @@
+const WebSocket = require('ws')
+const got = require('got')
+const EventEmitter = require('events')
+
+const wait = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+const parse = string => {
+  if (string === 'wait') {
+    return { empty: true }
+  }
+  return JSON.parse(string)
+}
+
+class DDAtHome extends EventEmitter {
+  constructor(url, { PARALLEL = 48, PING_INTERVAL = 1000 * 30, INTERVAL = 480, start = true } = {}) {
+    super()
+    this.url = url
+    this.PARALLEL = PARALLEL
+    this.PING_INTERVAL = PING_INTERVAL
+    this.INTERVAL = INTERVAL
+    this.stoped = false
+    this.queryTable = new Map()
+    if (start) {
+      this.start()
+    }
+  }
+
+  connect() {
+    const ws = new WebSocket(this.url)
+    this.ws = ws
+    return new Promise(resolve => {
+      const pending = []
+
+      const processer = async () => {
+        await wait(this.INTERVAL * this.PARALLEL * Math.random())
+        while (ws.readyState === 1) {
+          const now = Date.now()
+          const pause = wait(this.INTERVAL * this.PARALLEL)
+          const { key, url, empty } = await new Promise(resolve => {
+            pending.push(resolve)
+            this.secureSend('DDDhttp')
+          })
+          if (empty) {
+            this.emit('log', 'wait')
+          } else {
+            this.emit('log', 'job received', url)
+            this.emit('url', url)
+            const time = Date.now()
+            const { body } = await got(url).catch(e => ({ body: JSON.stringify({ code: e.statusCode }) }))
+            const result = this.secureSend(JSON.stringify({
+              key,
+              data: body
+            }))
+            if (result) {
+              this.emit('done')
+              this.emit('log', `job complete ${((Date.now() - time) / 1000).toFixed(2)}s`, this.INTERVAL * this.PARALLEL - Date.now() + now)
+            }
+          }
+          await pause
+        }
+      }
+
+      ws.on('message', message => {
+        const { key, data, empty } = parse(message)
+        if (empty) {
+          const resolve = pending.shift()
+          if (resolve) {
+            resolve({ empty })
+          }
+        } else if (data) {
+          const { type, url, result } = data
+          if (type === 'query') {
+            if (this.queryTable.has(key)) {
+              this.queryTable.get(key)(result)
+              this.queryTable.delete(key)
+            }
+          } else if (type === 'http') {
+            const resolve = pending.shift()
+            if (resolve) {
+              resolve({ key, url })
+            }
+          }
+        }
+      })
+
+      ws.on('open', async () => {
+        this.emit('log', 'DD@Home connected')
+        this.emit('open')
+        Array(this.PARALLEL).fill().map(processer)
+
+        let lastPong = Date.now()
+
+        ws.on('pong', () => {
+          this.emit('log', 'pong')
+          lastPong = Date.now()
+        })
+
+        while (ws.readyState === 1) {
+          ws.ping()
+          await wait(this.PING_INTERVAL)
+          if ((Date.now() - lastPong - this.PING_INTERVAL * 5) > 0) {
+            this.emit('log', 'timeout')
+            ws.close(4663, 'timeout')
+          }
+        }
+      })
+
+      ws.on('error', e => {
+        console.error(`error: ${e.message}`)
+      })
+
+      ws.on('close', n => {
+        this.emit('log', `closed ${n}`)
+        this.emit('close')
+        setTimeout(resolve, 1000)
+      })
+    })
+  }
+
+  secureSend(data) {
+    if (this.ws.readyState === 1) {
+      this.ws.send(data)
+      return true
+    }
+  }
+
+  ask(query) {
+    return new Promise((resolve, reject) => {
+      const key = String(Math.random())
+      this.queryTable.set(key, resolve)
+      this.secureSend(JSON.stringify({ key, query }))
+      setTimeout(() => {
+        if (this.queryTable.has(key)) {
+          reject(new Error('timeout'))
+        }
+      }, 1000 * 5)
+    })
+  }
+
+  stop() {
+    this.stoped = true
+    if (this.ws.readyState === 1) {
+      this.ws.close()
+    }
+  }
+
+  async start() {
+    while (!this.stoped) {
+      await this.connect()
+    }
+  }
+}
+
+module.exports = DDAtHome
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const { version: VERSION } = require('./package.json')
 const { URL } = require('url') // Compatibility
-const WebSocket = require('ws')
-const got = require('got')
+const DDAtHome = require('./core')
 
 console.log(`
 ${Array(process.stdout.columns).fill('D').join('')}
@@ -10,27 +9,8 @@ Please read README.md for more information.
 ${Array(process.stdout.columns).fill('D').join('')}
 `)
 
-const wait = ms => new Promise(resolve => setTimeout(resolve, ms))
-
-const parse = string => {
-  if (string === 'wait') {
-    return { empty: true }
-  }
-  try {
-    const json = JSON.parse(string)
-    if (json) {
-      const { key, data: { type, url } } = json
-      if (type === 'http') {
-        return { key, url }
-      }
-    }
-  } catch (_) {
-    return undefined
-  }
-}
 const url = new URL(process.env.url || (process.env.development ? 'ws://0.0.0.0:9013' : 'wss://cluster.vtbs.moe'))
 
-let done = 0
 const PARALLEL = 48
 const INTERVAL = Number.isNaN(Number(process.env.interval)) ? 480 : Number(process.env.interval)
 const PING_INTERVAL = 1000 * 30
@@ -70,95 +50,6 @@ console.log({
 
 console.log(`using: ${url}`)
 
-const connect = () => new Promise(resolve => {
-  const ws = new WebSocket(url)
+const ws = new DDAtHome(url, { PARALLEL, PING_INTERVAL, INTERVAL })
 
-  const pending = []
-
-  const secureSend = data => {
-    if (ws.readyState === 1) {
-      ws.send(data)
-      return true
-    }
-  }
-
-  const processer = async () => {
-    await wait(INTERVAL * PARALLEL * Math.random())
-    while (true) {
-      const now = Date.now()
-      const pause = wait(INTERVAL * PARALLEL)
-      const { key, url, empty } = await new Promise(resolve => {
-        pending.push(resolve)
-        secureSend('DDDhttp')
-      })
-      if (empty) {
-        log('wait')
-      } else {
-        const time = Date.now()
-        const { body } = await got(url).catch(e => ({ body: JSON.stringify({ code: e.statusCode }) }))
-        const result = secureSend(JSON.stringify({
-          key,
-          data: body
-        }))
-        if (result) {
-          if (verbose) {
-            done++
-          }
-          log(`job complete ${((Date.now() - time) / 1000).toFixed(2)}s`, Math.round(process.uptime() * 1000 / done), INTERVAL * PARALLEL - Date.now() + now)
-        }
-      }
-      await pause
-    }
-  }
-
-  ws.on('message', async message => {
-    const json = parse(message)
-    if (json) {
-      const resolve = pending.shift()
-      if (resolve) {
-        if (json.url) {
-          log('job received', json.url)
-        }
-        resolve(json)
-      }
-    }
-  })
-
-  ws.on('open', async () => {
-    log('DD@Home connected')
-    Array(PARALLEL).fill().map(processer)
-
-    let lastPong = Date.now()
-
-    ws.on('pong', () => {
-      log('pong')
-      lastPong = Date.now()
-    })
-
-    while (ws.readyState === 1) {
-      ws.ping()
-      await wait(PING_INTERVAL)
-      if ((Date.now() - lastPong - PING_INTERVAL * 5) > 0) {
-        log('timeout')
-        ws.close(4663, 'timeout')
-      }
-    }
-  })
-
-  ws.on('error', e => {
-    console.error(`error: ${e.message}`)
-  })
-
-  ws.on('close', n => {
-    log(`closed ${n}`)
-    setTimeout(resolve, 1000)
-  })
-})
-
-;
-
-(async () => {
-  while (true) {
-    await connect()
-  }
-})()
+ws.on('log', log)
