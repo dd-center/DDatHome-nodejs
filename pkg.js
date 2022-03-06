45494a14ccfdd4722a6e370c802d172d011927ef
simon3000 <w@simon3k.moe>
2022-03-06 10:52:21 +0800

    pkg node16

--- pkg.js
+++ pkg.js
@@ -3,9 +3,9 @@ const { exec } = require('pkg')
 ;
 
 [
-  ['node12-win-x64', '.exe'],
-  ['node12-macos-x64'],
-  ['node12-linux-x64']
+  ['node16-win-x64', '.exe'],
+  ['node16-macos-x64'],
+  ['node16-linux-x64']
 ].map(([target, e = '']) => ['index.js', '--target', target, '--output', `./dist/${target}${e}`])
   .map(params => async () => exec(params))
   .reduce((p, f) => p.then(f), Promise.resolve(233))
