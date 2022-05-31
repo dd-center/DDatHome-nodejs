ea4163a7c4638756af4a71b3ffbc20e992a77c6c
simon3000 <simon30002021@icloud.com>
2022-05-31 20:54:48 +0200

    use node 18

--- pkg.js
+++ pkg.js
@@ -3,9 +3,9 @@ const { exec } = require('pkg')
 ;
 
 [
-  ['node16-win-x64', '.exe'],
-  ['node16-macos-x64'],
-  ['node16-linux-x64']
+  ['node18-win-x64', '.exe'],
+  ['node18-macos-x64'],
+  ['node18-linux-x64']
 ].map(([target, e = '']) => ['index.js', '--target', target, '--output', `./dist/${target}${e}`])
   .map(params => () => exec(params))
   .reduce((p, f) => p.then(f), Promise.resolve(233))
